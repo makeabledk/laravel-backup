78605b8bafefad514cf9abe43447ec60a3eabb88
Rasmus Nielsen <rasmus@easify.dk>
2018-11-13 15:49:51 +0100

    Extract is-reachable-check to healtch check

---
+++ src/Tasks/Monitor/HealthChecks/IsReachable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Spatie\Backup\Tasks\Monitor\HealthChecks;
+
+use Spatie\Backup\BackupDestination\BackupDestination;
+use Spatie\Backup\Tasks\Monitor\HealthCheck;
+
+class IsReachable extends HealthCheck
+{
+    public function handle(BackupDestination $backupDestination)
+    {
+        $this->failUnless($backupDestination->isReachable(),
+            trans('backup::notification.unhealthy_backup_found_not_reachable', [
+                'error' => $backupDestination->connectionError
+            ])
+        );
+    }
+}
